b08cbe817b34cfca2e04e4a1b946f0355316971a
Miles D'Antonio <milesjdantonio@gmail.com>
2026-09-21 16:39:22 -0400

    Added Color Codes

--- BankOrBust/src/main/java/BustOrBank.java
+++ BankOrBust/src/main/java/BustOrBank.java
@@ -13,6 +13,16 @@ import java.util.*;
 
 public class BustOrBank {
    
+    public static final String COLOR_RESET = "\u001B[0m";
+    public static final String COLOR_RED = "\u001B[31m";
+    public static final String COLOR_ORANGE = "\u001B[38;5;214m";
+    public static final String COLOR_YELLOW = "\u001B[33m";
+    public static final String COLOR_GREEN = "\u001B[32m";
+    public static final String COLOR_BLUE = "\u001B[34m";
+    public static final String COLOR_PURPLE = "\u001B[35m";
+    public static final String COLOR_PINK = "\u001B[38;5;206m";
+    public static final String COLOR_BROWN = "\u001B[38;5;94m";
+
     public Scanner input = new Scanner(System.in);
 
 }
