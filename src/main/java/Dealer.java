92dc7dab4178a3e46ff991e10b3c3962b5007320
Miles D'Antonio <milesjdantonio@gmail.com>
2026-09-24 12:22:31 -0400

    Dealer Method

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -1,6 +1,41 @@
 public class Dealer extends Participant {
 
     public Dealer(String name) {
-        super(name);
+        super("Dealer");
     }
+
+    public boolean canHit()
+    {
+        return getHandValue() < 17;
+    }
+
+    public boolean getHard17()
+    {
+        if (getHandValue() != 17)
+        {
+            return false;
+        }
+
+        int total = 0;
+        int aceCount = 0;
+
+        for (int i = 0; i < getCardCount(); i++)
+        {
+            total += getHand()[i].getValue(this);
+
+            if (getHand()[i].isAce())
+            {
+                aceCount++;
+            }
+        }
+        while (total > 21 && aceCount > 0)
+        {
+            total -= 10;
+            aceCount--;
+        }
+        return aceCount == 0;
+
+    }
+
+
 }
